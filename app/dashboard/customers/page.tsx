ee0610377aa74c83f220cf795405bbf20e70e91f
Carlos Rangel <charlesleimaginant@gmail.com>
2024-01-27 22:08:34 -0600

    Add fetch data for Customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,23 @@
 import { Metadata } from 'next';
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
 
 export const metadata: Metadata = {
   title: 'Customers Page',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page() {
+  const customers = await fetchFilteredCustomers('');
+  console.log(customers);
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+          Customers
+        </h1>
+      </div>
+      <Table customers={customers} />
+    </div>
+  );
 }
